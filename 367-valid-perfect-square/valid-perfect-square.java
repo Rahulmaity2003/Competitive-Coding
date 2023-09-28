674f0c7cd4e505708fc2c3d3c81692d92bb5ac21
Rahul Maity <96173173+Rahulmaity2003@users.noreply.github.com>
2023-09-28 21:14:35 +0530

    Time: 1 ms (22.84%) | Memory: 39.2 MB (48.48%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,26 +1,35 @@
+// class Solution {
+//     public boolean isPerfectSquare(int num) {
+//         // long n = num;
+//         if(num==1){
+//             return true;
+//         }
+//          long i=0;
+//         long n = num;
+//         while(i<num){
+//             if(i*i == num){
+//                 return true;
+//             }
+//             i++;
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean isPerfectSquare(int num) {
-        // long n = num;
-        if(num==1){
+        if(num==1)
+        {
             return true;
         }
-         long i=0;
+        long i=0;
         long n = num;
-        while(i<num){
-            if(i*i == num){
+        while(i*i<=n)
+        {
+            if(i*i == n)
                 return true;
-            }
             i++;
         }
         return false;
     }
 }
-// class solution{
-//     public boolean isPerfectSquare(int num){
-//     if(num==1){
-//         return true;
-//     }
-//     int low = 0;
-//     int high = 
-//     }
-// }
